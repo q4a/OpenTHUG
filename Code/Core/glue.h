43aac73756ea1af94984ea239280ad5ee2837e76
q4a <q4arus@ya.ru>
2020-04-26 05:03:01 +0300

    move Code to src and add doxygen file

--- Code/Core/glue.h
+++
@@ -1,5 +0,0 @@
-#ifndef __GLUE_H__
-#define __GLUE_H__
-
-
-#endif		// __GLUE_H__
--- Code/Gfx/stdafx.h
+++
@@ -1,3 +0,0 @@
-// You cannot conditionally compile-out stdafx.h in the PC project
-// so the only recourse is to always include "stdafx.h" in both
-// projects and to include this dummy version on the PS2 project
